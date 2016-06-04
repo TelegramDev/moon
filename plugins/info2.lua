e1ca040fa395c951d4a8c6c4c9d6e9187228159f
TelegramDev <makan.j110110@gmail.com>
2016-06-05 04:09:12 +0430

    Update info2.lua

--- plugins/info2.lua
+++ plugins/info2.lua
@@ -446,6 +446,46 @@ local function run(msg, matches)
 			return res_user(matches[2]:gsub("@",""), callback_res, cbres_extra)
 		end
 	else
+                --ginfo ------------------------------------------------------------------------------------------------
+	info = "Full name: "..string.gsub(result.print_name, "_", " ").."\n"
+	.."First name: "..(result.first_name or "-----").."\n"
+	.."Last name: "..(result.last_name or "-----").."\n\n"
+	.."Phone number: "..number.."\n"
+	.."Username: @"..(result.username or "-----").."\n"
+	.."ID: "..result.id.."\n\n"
+        .."Global Rank "..usergrank.."\n"
+	.."Rank: "..usertype.."\n"
+	.."Position: "..userrank.."\n\n"
+	send_large_msg(org_chat_id, info)
+end
+
+local function run(msg, matches)
+	local data = load_data(_config.moderation.data)
+	org_chat_id = "chat#id"..msg.to.id
+	if is_sudo(msg) then
+		access = 1
+	else
+		access = 0
+	end
+	if matches[1] == '/rankdel' and is_sudo(msg) then
+		azlemagham = io.popen('rm ./rank/'..matches[2]..'.txt'):read('*all')
+		return 'done'
+	elseif matches[1] == '/rank' and is_sudo(msg) then
+		local name = string.sub(matches[2], 1, 50)
+		local text = string.sub(matches[3], 1, 10000000000)
+		local file = io.open("./rank/"..name..".txt", "w")
+		file:write(text)
+		file:flush()
+		file:close() 
+		return "done"
+	elseif #matches == 2 then
+		local cbres_extra = {chatid = msg.to.id}
+		if string.match(matches[2], '^%d+$') then
+			return user_info('user#id'..matches[2], callback_info, cbres_extra)
+		else
+			return res_user(matches[2]:gsub("@",""), callback_res, cbres_extra)
+		end
+	else
 		--custom rank ------------------------------------------------------------------------------------------------
 		local file = io.open("./info/"..msg.from.id..".txt", "r")
 		if file ~= nil then
@@ -495,7 +535,7 @@ local function run(msg, matches)
 			--number ------------------------------------------------------------------------------------------------
 			if msg.from.phone then
 				numberorg = string.sub(msg.from.phone, 3)
-				number = "****0"..string.sub(numberorg, 0,6)
+				number = "0*****"..string.sub(numberorg, 0,6)
 				if string.sub(msg.from.phone, 0,2) == '98' then
 					number = number.."\nLocation: Iran Islamic"
 					if string.sub(msg.from.phone, 0,4) == '9891' then
